38aa2a8a89954e5d3e95eae4dc2bb97f99f67eba
souravkp2005 <mt1230751@iitd.ac.in>
2024-07-04 16:45:48 +0530

    register user form added

--- market/__init__.py
+++ market/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+app.config['SECRET_KEY']='be6731ae2d39a95afe686021'
 db=SQLAlchemy(app)
+
+from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from market import app
-from market.models import Item
+from flask import render_template,redirect,url_for
+from market import app,db
+from market.models import Item, User
+from market.form import RegisterForm
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -10,3 +11,16 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html',items=items)
+
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+    form =RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address = form.email_address.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if(form.errors !={}):
+        for err_msg in form.errors.values():
+            print('Error : ',err_msg)
+    return render_template('register.html',form=form)
